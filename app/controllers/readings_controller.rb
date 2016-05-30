03d561728694471343f73be6ac739c3b44444dfd
Armando Andini <armando.andini@hotmail.com>
2016-05-29 23:23:32 -0300

    Change sorting of temp and hum values.

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -2,7 +2,7 @@ class ReadingsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @readings = Reading.order :created_at
+    @readings = Reading.order :created_at' => 'desc'
   end
 
   def create
