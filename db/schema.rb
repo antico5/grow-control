8aa5a362513c399382c5ab07ce548e4cced1fd9d
Armando Andini <armando.andini@hotmail.com>
2016-08-18 09:00:26 -0300

    Run migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528183504) do
+ActiveRecord::Schema.define(version: 20160818115710) do
 
   create_table "failed_readings", force: :cascade do |t|
     t.string   "grow_name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160528183504) do
     t.string   "temperature"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "grows", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "start_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "readings", force: :cascade do |t|
