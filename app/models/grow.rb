fcc4e42df72469468facd3c55844841770d1a378
Armando Andini <armando.andini@hotmail.com>
2016-08-19 15:02:25 -0300

    Refactor chart data genereation on Grow model.

--- app/models/grow.rb
+++ app/models/grow.rb
@@ -1,11 +1,19 @@
 class Grow < ActiveRecord::Base
   has_many :readings
 
-  def reading_data_for_chart
-    selected_readings = readings.select(:created_at, :temperature, :humidity).limit 168
-    labels = selected_readings.map {|reading| reading.created_at.to_formatted_s :short }
-    values = selected_readings.map &:temperature
-    { labels: labels, values: values }
+  def chart_data readings
+    { created_at: readings.map { |reading| reading.created_at.to_formatted_s :short },
+      temperatures: readings.pluck(:temperature),
+      humidities: readings.pluck(:humidity) }
   end
+
+  def readings_for_week week
+    week -= 1
+    from_date = start_date + week.weeks
+    to_date = from_date + 1.week
+    readings.where(created_at: from_date .. to_date)
+      .select(:created_at, :temperature, :humidity)
+  end
+
 
 end
