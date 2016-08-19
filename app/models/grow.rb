b50f194ab62106fd7373ef24381d3952e0285a28
Armando Andini <armando.andini@hotmail.com>
2016-08-19 13:13:49 -0300

    Grow#reading_data_for_chart method.

--- app/models/grow.rb
+++ app/models/grow.rb
@@ -1,3 +1,11 @@
 class Grow < ActiveRecord::Base
   has_many :readings
+
+  def reading_data_for_chart
+    selected_readings = readings.select(:created_at, :temperature, :humidity).limit 168
+    labels = selected_readings.map {|reading| reading.created_at.to_formatted_s :short }
+    values = selected_readings.map &:temperature
+    { labels: labels, values: values }
+  end
+
 end
